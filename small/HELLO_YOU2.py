4c95b105aab273b347d74215bf2e81804b27125a
mculep <mculep@gmail.com>
2020-10-14 20:59:05 -0700

    small exercises

---
+++ small/HELLO_YOU2.py
@@ -0,0 +1,8 @@
+
+
+name = input("WHAT IS YOUR NAME?\n")
+length = len(name)
+
+print(f"HELLO {name.upper()}")
+
+print(f"YOUR NAME HAS {length} LETTERS IN IT! AWESOME!")
---
+++ small/madlib.py
@@ -0,0 +1,13 @@
+
+
+print("Please fill in the blanks below:\n___name___'s favorite subject in school is ___(subject)___.")
+
+name = input("Insert name\n")
+
+subject = input("Insert favorite subject in school:\n")
+
+#print(f"What is your {name}?")
+
+#print(f"What is your favorite {subject}")
+
+print (f"{name}'s favorite subject in school is {subject} .")
