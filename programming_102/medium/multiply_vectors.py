dd80f9d812dae7c58a3ad48967a5cae2557a8387
Melody Ulep <71471577+mculep@users.noreply.github.com>
2020-10-21 18:30:23 -0700

    Update multiply_vectors.py

--- programming_102/medium/multiply_vectors.py
+++ programming_102/medium/multiply_vectors.py
@@ -15,7 +15,7 @@ for i in range(len(things)):
 
 # 1. Multiply Vectors
 #Given two lists of numbers of the same length, create a new list by multiplying the pairs of numbers in corresponding positions in the two lists. Example:
-
+#[2, 4, 5] x [2, 3, 6] = [4, 12, 30]
 
 
 num1 = [2,4,5]
