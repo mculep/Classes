0cd025107941f2085c3c477d365695bc32e2e982
Melody Ulep <71471577+mculep@users.noreply.github.com>
2020-11-26 01:47:52 -0800

    Update hello-world.js

--- Node/hello-world.js
+++ Node/hello-world.js
@@ -3,9 +3,9 @@
 // 1. Console log from 1 to 100 using node.
 
 for (let i = 0; i <= 100; i++) {
-}
+    
 console.log(i)
-
+}
 // 2. Create a function that accepts a string as an argument.
 // Have the program write to the terminal "The cow says ... {the value supplied}"
 // Call that function 3 times with 3 different strings as the argument.
