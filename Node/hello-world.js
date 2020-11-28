38230ca98fdffc90413798a385671f292828fcac
Melody Ulep <71471577+mculep@users.noreply.github.com>
2020-11-27 23:57:12 -0800

    Update hello-world.js

--- Node/hello-world.js
+++ Node/hello-world.js
@@ -9,7 +9,7 @@ for (let i = 1; i < 101; i++) {
     
 
 
-}
+
 // 2. Create a function that accepts a string as an argument.
 // Have the program write to the terminal "The cow says ... {the value supplied}"
 // Call that function 3 times with 3 different strings as the argument.
