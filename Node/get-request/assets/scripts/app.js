f8e036ec4c4dad798beccabda38d0939d1e2dec7
mculep <mculep@gmail.com>
2020-11-28 21:23:08 -0800

    update exercise

--- Node/get-request/assets/scripts/app.js
+++ Node/get-request/assets/scripts/app.js
@@ -1,3 +1,7 @@
+// *********** Udemy Node Practice *************
+const listElement = document.querySelector(".posts");
+const singlePost = document.getElementById("single-post");
+
 const xhr = new XMLHttpRequest();
 // configuring the request
 xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
@@ -7,11 +11,13 @@ xhr.responseType = "json"; // this will automatically parse for you
 xhr.onload = function () {
     // This function handles the incoming response
     // Use this instead of event listeners (some browsers my not support it)
-    const listOfPost = JSON.parse(xhr.response); // <- you can do this instead of xhr.response.Type = 'json';
+    // const listOfPost = JSON.parse(xhr.response); // <- you can do this instead of xhr.response.Type = 'json';
     const listOfPost = xhr.response; // extracting the data or use
-    console.log(listOfPost);
+    for (const post of listOfPost) {
+        const postEl = document.importNode(singlePost.content, true);
+        postEl.querySelector("h2").textContent = post.title.toUpperCase(); //<- Files that we fetch shows title and a body property
+        postEl.querySelector("p").textContent = post.body;
+        listElement.append(postEl);
+    }
 };
 xhr.send();
-
-const listElement = document.querySelector(".post");
-const singlePost = document.getElementById("single-post");
